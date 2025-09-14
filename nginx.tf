0cf549369ca0a465272fad870bfb5adb6377e50e
Sebastian Gilian Alayo <sebastiangilian7@gmail.com>
2025-09-14 18:45:00 -0500

    feat(apps): declarar apps (1, 2 y 3) en app_net

---
+++ nginx.tf
@@ -0,0 +1,33 @@
+resource "docker_container" "app1" {
+  name  = "app1"
+  image = "nginx:stable-perl"
+  ports {
+    internal = 80
+    external = 8081
+  }
+  networks_advanced {
+    name = docker_network.app_net.name
+  }
+}
+resource "docker_container" "app2" {
+  name  = "app2"
+  image = "nginx:stable-perl"
+  ports {
+    internal = 80
+    external = 8082
+  }
+  networks_advanced {
+    name = docker_network.app_net.name
+  }
+}
+resource "docker_container" "app3" {
+  name  = "app3"
+  image = "nginx:stable-perl"
+  ports {
+    internal = 80
+    external = 8083
+  }
+  networks_advanced {
+    name = docker_network.app_net.name
+  }
+}
