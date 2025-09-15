30b59dc2b05c77b37625e277c83f2aaadc0d76ef
Sebastian Gilian Alayo <sebastiangilian7@gmail.com>
2025-09-14 21:44:08 -0500

    fix(nginx): cambiar version de nginx para utilizar el comando ping

--- nginx.tf
+++ nginx.tf
@@ -1,6 +1,6 @@
 resource "docker_container" "app1" {
   name  = "app1"
-  image = "nginx:stable-perl"
+  image = "nginx:stable-alpine3.21-perl"
   ports {
     internal = 80
     external = 8081
@@ -14,7 +14,7 @@ resource "docker_container" "app1" {
 }
 resource "docker_container" "app2" {
   name  = "app2"
-  image = "nginx:stable-perl"
+  image = "nginx:stable-alpine3.21-perl"
   ports {
     internal = 80
     external = 8082
@@ -28,7 +28,7 @@ resource "docker_container" "app2" {
 }
 resource "docker_container" "app3" {
   name  = "app3"
-  image = "nginx:stable-perl"
+  image = "nginx:stable-alpine3.21-perl"
   ports {
     internal = 80
     external = 8083
