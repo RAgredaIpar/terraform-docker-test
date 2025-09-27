f90ccb0baced7c65705453b9f19daba16110b3ff
RAgredaIpar <r.agredaipar@gmail.com>
2025-09-27 11:31:33 -0500

    chore(terraform): añadir variables y tfvars basados en workspace

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,11 @@
+grafana_external_port = {
+  dev  = 3000
+  qa   = 4000
+  prod = 5000
+}
+
+nginx_proxy_external_port = {
+  dev   = 5001
+  qa    = 5002
+  prod  = 5003
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "grafana_external_port" {
+  description = "Puerto externo de grafana"
+}
+
+variable "nginx_proxy_external_port" {
+  description = "Puerto externo del nginx proxy"
+}
